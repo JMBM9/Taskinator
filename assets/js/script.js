2bbd5d8e3b3e839a5fb0bf9ed6a26eecd6096c2c
Your Name <drjpolanco25@yahoo.com>
2022-05-17 15:33:01 -0700

    form submit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,11 @@
-var formEl = document.querySelector("#task-form");
-
+var buttonEl = document.querySelector("#save-task");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function(event){
-
-    event.preventDefault();
- 
-    var listItemEl=document.createElement("li");
-    listItemEl.className = "task-item";
-    listItemEl.textContent = "this is a new task.";
-tasksToDoEl.appendChild(listItemEl);
+var createTaskHandler = function() {
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+  listItemEl.textContent = "This is a new task.";
+  tasksToDoEl.appendChild(listItemEl);
 };
 
-
-formEl.addEventListener("click", createTaskHandler); 
-
+buttonEl.addEventListener("click", createTaskHandler);
